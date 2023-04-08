ea769b600900cb9b2ebfb58dcb76fade8a48f9ad
lsankar4033 <lsankar4033@gmail.com>
2023-04-07 20:34:11 -0700

    take sig strings as input to input builder instead of Signer

--- packages/circuits/tests/test_utils.ts
+++ packages/circuits/tests/test_utils.ts
@@ -6,7 +6,9 @@ import { Wallet } from "ethers";
 
 import { computeEffEcdsaPubInput } from "@personaelabs/spartan-ecdsa";
 
-// NOTE: OBVIOUSLY DON'T USE THIS ANYWHERE!
+import { buildPoseidon } from "circomlibjs";
+
+// NOTE: OBVIOUSLY DON'T USE THIS ANYWHERE! IT'S COMPROMISED.AF
 export const testPrivateKey =
   "0x58d23b55bc9cdce1f18c2500f40ff4ab7245df9a89505e9b1fa4851f623d241d";
 export const testSigner = new Wallet(testPrivateKey);
@@ -19,10 +21,14 @@ export type EffECDSASig = {
   s: bigint; // private
 };
 
-export type NymInput = {
+export type MerkleProof = {
   root: bigint;
-  pathIndices: number[]; // private
-  siblings: bigint[]; // private
+  siblings: bigint[];
+  pathIndices: number[];
+};
+
+export type NymInput = {
+  membershipProof: MerkleProof;
 
   nym: string;
   nymHash: string;
@@ -36,10 +42,11 @@ function bufferToBigInt(buf: Buffer): bigint {
   return BigInt("0x" + buf.toString("hex"));
 }
 
-// NOTE: return type here should be something that has Ts, Us, S
-async function computeSig(signer: Wallet, msg: string): Promise<EffECDSASig> {
-  const sig = await signer.signMessage(msg);
-  const { v, r: _r, s: _s } = fromRpcSig(sig);
+async function computeEffECDSASig(
+  sigStr: string,
+  msg: string
+): Promise<EffECDSASig> {
+  const { v, r: _r, s: _s } = fromRpcSig(sigStr);
 
   const r = bufferToBigInt(_r);
   const s = bufferToBigInt(_s);
@@ -51,19 +58,24 @@ async function computeSig(signer: Wallet, msg: string): Promise<EffECDSASig> {
   return { Tx, Ty, Ux, Uy, s };
 }
 
-// TODO: figure out merkle root input
+// NOTE: this method may not need be exposed, just generate proof
 export async function computeNymInput(
-  signer: Wallet,
+  membershipProof: MerkleProof,
+
   content: string,
-  nym: string
+  contentSigStr: string,
+
+  nym: string,
+  nymSigStr: string
 ): Promise<NymInput> {
-  const nymSig = await computeSig(signer, nym);
-  const contentSig = await computeSig(signer, content);
+  const nymSig = await computeEffECDSASig(nymSigStr, nym);
+  const contentSig = await computeEffECDSASig(contentSigStr, content);
+
+  // TODO: hash nymSig piece
+  // poseidon(nymSig.s, nymSig.s)
 
   return {
-    root: BigInt(0),
-    pathIndices: [],
-    siblings: [],
+    membershipProof,
 
     nym,
     nymHash: nym,
@@ -73,3 +85,5 @@ export async function computeNymInput(
     contentSig,
   };
 }
+
+// TODO: generate proof method
