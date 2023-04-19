8414da3e4132af858bf7717287a3636ba191283f
amirgamil <amir.amgad01@gmail.com>
2023-04-19 17:24:03 -0400

    feat: add some styles, hard-code some comments

--- packages/frontend/src/components/MessageRow.tsx
+++ packages/frontend/src/components/MessageRow.tsx
@@ -5,6 +5,7 @@ const relativeTime = require("dayjs/plugin/relativeTime");
 dayjs.extend(relativeTime);
 
 interface ICommentViewProps {
+    commentId: string;
     message: string;
     createdAt: Date;
     tagName: string;
@@ -12,7 +13,7 @@ interface ICommentViewProps {
     proof: string;
 }
 
-export const CommentView = ({ message, proof, createdAt, tagName, profileImgURL }: ICommentViewProps) => {
+export const CommentView = ({ message, proof, createdAt, tagName, profileImgURL, commentId }: ICommentViewProps) => {
     const [isOpen, setIsOpen] = React.useState(false);
     // TODO: after we add wagmi
     //  const { address } = useAccount();
@@ -32,13 +33,34 @@ export const CommentView = ({ message, proof, createdAt, tagName, profileImgURL 
     return (
         <>
             <MessageModal
-                description={prop.description}
-                propId={prop.id}
+                title={message}
+                commentId={commentId}
                 isOpen={isOpen}
                 handleClose={(e) => {
                     setIsOpen(false);
                 }}
             />
+            <div
+                onClick={openModal}
+                className="rounded-2xl transition-all shadow-sm bg-white p-3 md:px-5 md:py-4 flex flex-col gap-4 justify-between border border-gray-200 hover:border-gray-300 hover:cursor-pointer w-full"
+            >
+                <div className="text-lg md:text-xl font-bold self-start line-clamp-2">
+                    <span className="text-black tracking-tight font-bold">{message}</span>
+                </div>
+                <span>{message}</span>
+                <div className="flex justify-between items-center">
+                    <div className="flex justify-center items-center">
+                        <p className="font-bold">{tagName}</p>
+                        <div className="px-2"></div>
+                        <p>{dateFromDescription}</p>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <p>43 replies</p>
+                        <div className="px-2"></div>
+                        <p>2212 views</p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
