10f9ddd5cb4ff49b54c24fc6435e688486412f79
Dan Tehrani <contact@dantehrani.com>
2023-05-04 09:47:07 +0200

    Integrate backend w/ nymjs & Add script to populate post test data (#39)
    * chore: add SPECIFICATION.md
    
    * feat: implement the spec
    
    * feat: use nymjs in the backend
    
    * feat: update example frontend to use nymjs
    
    * feat: add utilities for Upvote
    
    * feat: add `groupRoot` to `Content` and `Upvote`
    
    * chore: cleanup
    
    * feat: check the consistency of contentMessage.groupRoot and publicInput.groupRoot in verification
    
    * chore: cleanup the spec
    
    * feat: add type `PrefixedHex`
    
    * chore: cleanup
    
    * chore: add spec title
    
    * chore: update util functions to recover the pubkey instead of the address
    
    the address can be derived from the pubkey if necessary
    
    * chore: rename `Content` to `Post`, and `ContentMessage` to `Content`
    
    * chore: apply the renaming to nymjs
    
    * feat: apply the new spec to the db scheme
    
    * feat: apply the new spec to the backend
    
    * feat: add example usage of nymjs and the API endpoints
    
    * chore: update api-doc to follow the new spec
    
    * chore: update the recursive query to reflect the latest schema
    
    * chore: build nymjs on install
    
    * chore: remove unusued root dependency
    
    * feat: add script to populate posts/upvotes tables with test data
    
    * fix: fix pnpm-lock.yaml (ran `pnpm i` in frontend/)
    
    * feat: update pre-commit to check the prisma schema in test_data/
    
    * chore: add `prepare` script which creates the .env file
    
    * chore: add some logs in the script
    
    * chore: run `prisma generate` at `prepare`
    
    * feat: make test data non-deterministic (since proofs are non-deterministic)
    
    * feat: create more dummy upvotes
    
    * feat: return the number of upvotes in recursive query
    
    ---------
    
    Co-authored-by: cha0sg0d <0xcha0sg0d@gmail.com>

--- packages/frontend/src/pages/api/v1/posts/[postId].ts
+++ packages/frontend/src/pages/api/v1/posts/[postId].ts
@@ -4,37 +4,65 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 // Return a single post and all of its children
 const handleGetPost = async (req: NextApiRequest, res: NextApiResponse) => {
   const posts: any[] = await prisma.$queryRaw`
-  WITH RECURSIVE thread AS (
-	SELECT
-		"id",
-		"parentId",
-		"address",
-		"title",
-		"body",
-		"createdAt"
-	FROM
-		"Post"
-	WHERE
-		"id" = ${req.query.postId}
-	UNION
-	SELECT
-		d. "id",
-		d. "parentId",
-		d. "address",
-		d. "title",
-		d. "body",
-		d. "createdAt"
-	FROM
-		"Post" d
-		INNER JOIN thread t ON d. "parentId" = t. "id"
-    )
-    SELECT
-    	*
-    FROM
-    	thread;
+	WITH RECURSIVE all_posts AS (
+			SELECT
+				posts. "id",
+				"title",
+				"body",
+				"parentId",
+				count("postUpvotes". "id") AS "upvotes",
+				posts. "timestamp"
+			FROM
+				"NymPost" posts
+			LEFT JOIN "DoxedUpvote" "postUpvotes" ON posts.id = "postUpvotes". "postId"
+		GROUP BY
+			posts. "id"
+		UNION
+		SELECT
+			posts. "id",
+			"title",
+			"body",
+			"parentId",
+			count("postUpvotes". "id") AS "upvotes",
+			posts. "timestamp"
+		FROM
+			"DoxedPost" posts
+			LEFT JOIN "DoxedUpvote" "postUpvotes" ON posts.id = "postUpvotes". "postId"
+		GROUP BY
+			posts.id
+		),
+		thread AS (
+			SELECT
+				"id",
+				"title",
+				"body",
+				"parentId",
+				"upvotes",
+				"timestamp"
+			FROM
+				all_posts
+			WHERE
+				"id" = ${req.query.postId}
+			UNION
+			SELECT
+				d. "id",
+				d. "title",
+				d. "body",
+				d. "parentId",
+				d. "upvotes",
+				d. "timestamp"
+			FROM
+				all_posts d
+				INNER JOIN thread t ON d. "parentId" = t. "id"
+		)
+		SELECT
+			*
+		FROM
+			thread;
   `;
 
-  res.send(posts);
+  // `upvotes` is in BigInt, so we need to convert it to a number
+  res.send(posts.map((post) => ({ ...post, upvotes: parseInt(post.upvotes) })));
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
