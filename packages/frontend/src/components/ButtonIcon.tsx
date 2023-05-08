f6bf4b22069fd1aa868e3951af74ef5f306a1a12
amir <amir.amgad01@gmail.com>
2023-05-08 14:52:51 -0400

    Add SVG support for inline button icons (#64)
    * feat: add images to threaded comments
    
    * feat: add react-inline-svg and programmatically change svg colors
    
    * chore: fix build

--- packages/frontend/src/components/ButtonIcon.tsx
+++ packages/frontend/src/components/ButtonIcon.tsx
@@ -1,21 +1,42 @@
-import Image from 'next/image';
+import SVG from 'react-inlinesvg';
+import styled from 'styled-components';
 
 interface IButtonProps {
   onClick: () => void;
   iconPath: string;
   iconWidth: number;
+  hoverBgColor?: string;
+  bgColor?: string;
   iconHeight: number;
   iconText: string;
 }
 
-export const ButtonIcon = ({ iconPath, iconWidth, iconHeight, iconText }: IButtonProps) => {
+export const ButtonContainer = styled.button<{ hoverBgColor?: string; bgColor?: string }>`
+  &:hover {
+    svg {
+      color: ${(props) => props.hoverBgColor || 'transparent'};
+    }
+  }
+  svg {
+    color: ${(props) => props.bgColor || 'transparent'};
+  }
+`;
+
+export const ButtonIcon = ({
+  iconPath,
+  iconWidth,
+  hoverBgColor,
+  bgColor,
+  iconHeight,
+  iconText,
+}: IButtonProps) => {
   return (
-    <button>
+    <ButtonContainer hoverBgColor={hoverBgColor} bgColor={bgColor}>
       <div className="flex justify-center items-center">
-        <Image src={iconPath} width={iconWidth} height={iconHeight} alt="reply" />
+        <SVG src={iconPath} height={iconHeight} width={iconWidth} fill={bgColor} />
         <div className="px-1"></div>
         <p style={{ color: 'gray' }}>{iconText}</p>
       </div>
-    </button>
+    </ButtonContainer>
   );
 };
