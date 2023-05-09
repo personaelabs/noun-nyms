053ee88443132c2d4b813c4e9222f7ab681cc1fe
lsankar4033 <lsankar4033@gmail.com>
2023-05-09 16:45:16 -0700

    fix: temporarily remove example pages/components

--- packages/frontend/src/components/example/PostMessage.tsx
+++ packages/frontend/src/components/example/PostMessage.tsx
@@ -1,265 +1,252 @@
-import { useEffect, useState } from 'react';
-import axiosBase from 'axios';
-import {
-  NYM_CODE_TYPE,
-  NymProver,
-  eip712MsgHash,
-  DOMAIN,
-  Content,
-  AttestationScheme,
-  toTypedNymCode,
-  CONTENT_MESSAGE_TYPES,
-  PrefixedHex,
-  EIP712TypedData,
-} from '@personaelabs/nymjs';
-import { MerkleProof } from '@personaelabs/spartan-ecdsa';
-import { ecrecover, fromRpcSig } from '@ethereumjs/util';
-import { useSignTypedData, useAccount } from 'wagmi';
+// import { useEffect, useState } from 'react';
+// import axiosBase from 'axios';
+// import {
+//   NymProver,
+//   eip712MsgHash,
+//   DOMAIN,
+//   Content,
+//   AttestationScheme,
+//   toTypedNymCode,
+//   CONTENT_MESSAGE_TYPES,
+//   PrefixedHex,
+//   EIP712TypedData,
+//   NYM_CODE_TYPE,
+// } from '@personaelabs/nymjs';
+// import { MerkleProof } from '@personaelabs/spartan-ecdsa';
+// import { ecrecover, fromRpcSig } from '@ethereumjs/util';
 
-const axios = axiosBase.create({
-  baseURL: `http://localhost:3000/api/v1`,
-});
+// import { useSignTypedData } from 'wagmi';
 
-type Member = {
-  pubkey: string;
-  path: string[];
-  indices: number[];
-};
+// const axios = axiosBase.create({
+//   baseURL: `http://localhost:3000/api/v1`,
+// });
 
-type ContentUserInput = {
-  title: string;
-  body: string;
-  parentId: PrefixedHex;
-};
+// type Member = {
+//   pubkey: string;
+//   path: string[];
+//   indices: number[];
+// };
 
-const submitPost = async (
-  content: Content,
-  attestation: string,
-  attestationScheme: AttestationScheme,
-) => {
-  const result = await axios.post(`/posts`, {
-    content,
-    attestation,
-    attestationScheme,
-  });
+// type ContentUserInput = {
+//   title: string;
+//   body: string;
+//   parentId: PrefixedHex;
+// };
 
-  return result;
-};
+// const submitPost = async (
+//   content: Content,
+//   attestation: string,
+//   attestationScheme: AttestationScheme,
+// ) => {
+//   const result = await axios.post(`/posts`, {
+//     content,
+//     attestation,
+//     attestationScheme,
+//   });
 
-const getLatestGroup = async () => {
-  const { data } = await axios.get('/groups/latest?set=1');
+//   return result;
+// };
 
-  const group: {
-    root: PrefixedHex;
-    members: Member[];
-  } = data;
+// const getLatestGroup = async () => {
+//   const { data } = await axios.get('/groups/latest?set=1');
 
-  return group;
-};
+//   const group: {
+//     root: PrefixedHex;
+//     members: Member[];
+//   } = data;
 
-const getPubKeyFromEIP712Sig = (typedData: EIP712TypedData, sig: string): string => {
-  const { v, r, s } = fromRpcSig(sig);
-  return `0x${ecrecover(
-    eip712MsgHash(typedData.domain, typedData.types, typedData.value),
-    v,
-    r,
-    s,
-  ).toString('hex')}`;
-};
+//   return group;
+// };
 
-const ExamplePost = () => {
-  const { address } = useAccount();
-  const [hydrated, setHydrated] = useState(false);
-  useEffect(() => {
-    setHydrated(true);
-  }, []);
+// const getPubKeyFromEIP712Sig = (typedData: EIP712TypedData, sig: string): string => {
+//   const { v, r, s } = fromRpcSig(sig);
+//   return ecrecover(
+//     eip712MsgHash(typedData.domain, typedData.types, typedData.value),
+//     v,
+//     r,
+//     s,
+//   ).toString('hex');
+// };
 
-  const [nymCode, setNymCode] = useState<string>('');
-  const [doxedContentInput, setDoxedContentInput] = useState<ContentUserInput>({
-    title: 'This is the title of a doxed post',
-    body: 'This is the body of a doxed post',
-    parentId: '0x0',
-  });
+// const ExamplePost = () => {
+//   const { address } = useAccount();
+//   const [hydrated, setHydrated] = useState(false);
+//   useEffect(() => {
+//     setHydrated(true);
+//   }, []);
 
-  const [nymContentInput, setNymContentInput] = useState<ContentUserInput>({
-    title: 'This is the title of a Nym post',
-    body: 'This is the body of a Nym post',
-    parentId: '0x0',
-  });
+//   const [nymCode, setNymCode] = useState<string>('');
+//   const [contentInput, setContentInput] = useState<ContentUserInput>({
+//     title: "I'm Satoshi Nakamoto",
+//     body: "I'm the creator of Bitcoin.",
+//     parentId: '0x0',
+//   });
 
-  const { data: nymSig, signTypedData: signNymCode } = useSignTypedData({
-    domain: DOMAIN,
-    types: NYM_CODE_TYPE,
-    value: {
-      nymCode,
-    },
-  });
+//   const domain = {
+//     name: 'Ether Mail',
+//     version: '1',
+//     chainId: 1,
+//     verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+//   } as NewType;
 
-  const { signTypedDataAsync } = useSignTypedData();
+//   const { data: nymSig, signTypedData: signNymCode } = useSignTypedData({
+//     primaryType: 'Document' as string | number,
+//     domain: DOMAIN,
+//     types: NYM_CODE_TYPE,
+//     message: {
+//       nymCode,
+//     },
+//   });
 
-  const postPseudo = async () => {
-    if (nymCode && nymSig) {
-      const group = await getLatestGroup();
-      const typedNymCode = toTypedNymCode(nymCode);
-      const userPubKey = getPubKeyFromEIP712Sig(typedNymCode, nymSig);
+//   const { signTypedDataAsync } = useSignTypedData();
 
-      // Get the user's merkle proof
-      const userMerkleProof = group.members.find((member) => member.pubkey === userPubKey);
+//   const postPseudo = async () => {
+//     if (nymCode && nymSig) {
+//       const group = await getLatestGroup();
+//       const typedNymCode = toTypedNymCode(nymCode);
+//       const userPubKey = getPubKeyFromEIP712Sig(typedNymCode, nymSig);
 
-      // Throw if merkle proof not found (implies user not in set)
-      if (!userMerkleProof) {
-        throw new Error('User not found in set');
-      }
+//       // Get the user's merkle proof
+//       const userMerkleProof = group.members.find((member) => member.pubkey === userPubKey);
 
-      const merkleProof: MerkleProof = {
-        pathIndices: userMerkleProof?.indices,
-        siblings: userMerkleProof?.path.map((sibling) => BigInt(sibling)),
-        root: BigInt(group.root),
-      };
+//       // Throw if merkle proof not found (implies user not in set)
+//       if (!userMerkleProof) {
+//         throw new Error('User not found in set');
+//       }
 
-      // Construct the content to sign
-      const content: Content = {
-        venue: 'nouns',
-        title: nymContentInput.title,
-        body: nymContentInput.body,
-        parentId: nymContentInput.parentId ? nymContentInput.parentId : '0x0',
-        groupRoot: group.root,
-        timestamp: Math.round(Date.now() / 1000),
-      };
+//       const merkleProof: MerkleProof = {
+//         pathIndices: userMerkleProof?.indices,
+//         siblings: userMerkleProof?.path.map((sibling) => BigInt('0x' + sibling)), // TODO: Use PrefixedHex in the backend as well
+//         root: BigInt('0x' + group.root),
+//       };
 
-      // Request the user to sign the content
-      const contentSig = await signTypedDataAsync({
-        domain: DOMAIN,
-        types: CONTENT_MESSAGE_TYPES,
-        value: content,
-      });
+//       // Construct the content to sign
+//       const content: Content = {
+//         venue: 'nouns',
+//         title: contentInput.title,
+//         body: contentInput.body,
+//         parentId: contentInput.parentId,
+//         groupRoot: ('0x' + group.root) as PrefixedHex,
+//         timestamp: Math.round(Date.now() / 1000),
+//       };
 
-      // Setup the prover
-      const nymProver = new NymProver({
-        enableProfiler: true,
-      });
+//       const types = {
+//         Person: [
+//           { name: 'name', type: 'string' },
+//           { name: 'wallet', type: 'address' },
+//         ],
+//         Mail: [
+//           { name: 'from', type: 'Person' },
+//           { name: 'to', type: 'Person' },
+//           { name: 'contents', type: 'string' },
+//         ],
+//       } as const;
 
-      await nymProver.initWasm();
+//       // Request the user to sign the content
+//       const contentSig = await signTypedDataAsync({
+//         domain: DOMAIN,
+//         types,
+//         value: content,
+//       });
 
-      // Generate the proof
-      const attestation = await nymProver.prove(
-        nymCode,
-        content,
-        nymSig as string,
-        contentSig as string,
-        merkleProof,
-      );
+//       // Setup the prover
+//       const nymProver = new NymProver({
+//         enableProfiler: true,
+//       });
 
-      const attestationHex = Buffer.from(attestation).toString('hex');
+//       await nymProver.initWasm();
 
-      const result = await submitPost(content, attestationHex, AttestationScheme.Nym);
-      console.log('Created a pseudonymous post! postId', result.data.postId);
-    }
-  };
+//       // Generate the proof
+//       const attestation = await nymProver.prove(
+//         nymCode,
+//         content,
+//         nymSig as string,
+//         contentSig as string,
+//         merkleProof,
+//       );
 
-  const postDoxed = async () => {
-    const group = await getLatestGroup();
+//       const attestationHex = Buffer.from(attestation).toString('hex');
 
-    // Construct the content to sign
-    const content: Content = {
-      venue: 'nouns',
-      title: doxedContentInput.title,
-      body: doxedContentInput.body,
-      parentId: doxedContentInput.parentId ? doxedContentInput.parentId : '0x0',
-      groupRoot: group.root,
-      timestamp: Math.round(Date.now() / 1000),
-    };
+//       const result = await submitPost(content, attestationHex, AttestationScheme.Nym);
+//       console.log('Created a pseudonymous post! postId', result.data.postId);
+//     }
+//   };
 
-    // Request the user to sign the content
-    const attestation = await signTypedDataAsync({
-      domain: DOMAIN,
-      types: CONTENT_MESSAGE_TYPES,
-      value: content,
-    });
+//   const postDoxed = async () => {
+//     const group = await getLatestGroup();
 
-    const result = await submitPost(content, attestation, AttestationScheme.EIP712);
+//     // Construct the content to sign
+//     const content: Content = {
+//       venue: 'nouns',
+//       title: contentInput.title,
+//       body: contentInput.body,
+//       parentId: contentInput.parentId,
+//       groupRoot: ('0x' + group.root) as PrefixedHex,
+//       timestamp: Math.round(Date.now() / 1000),
+//     };
 
-    console.log('Created a non-pseudonymous post! postId', result.data.postId);
-  };
+//     // Request the user to sign the content
+//     const attestation = await signTypedDataAsync({
+//       domain: DOMAIN,
+//       types: CONTENT_MESSAGE_TYPES,
+//       value: content,
+//     });
 
-  return (
-    <div className="flex flex-row gap-16 mt-4 justify-center">
-      <div>
-        <p className="text-xl">Doxed post</p>
-        <p className="text-gray-700 font-bold mb-2 block mt-4">Your address</p>
-        <p className="text-gray-700 font-bold mb-2 block">{hydrated && address}</p>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={postDoxed}
-        >
-          Submit a doxed post
-        </button>
-        <div className="py-2 mt-4">
-          <input
-            id="input"
-            type="text"
-            placeholder="Parent ID (optional)"
-            className="border border-gray-400 p-3 rounded-lg focus:outline-none focus:border-blue-500"
-            value={doxedContentInput.parentId === '0x0' ? '' : doxedContentInput.parentId}
-            onChange={(e) =>
-              setDoxedContentInput({
-                ...doxedContentInput,
-                parentId: e.target.value as PrefixedHex,
-              })
-            }
-          />
-        </div>
-      </div>
+//     const result = await submitPost(content, attestation, AttestationScheme.EIP712);
 
-      <div>
-        <p className="text-xl">Pseudonymous post</p>
-        <div className="relative mt-4">
-          <label className="text-gray-700 font-bold mb-2 block">Your Nym</label>
-          <input
-            id="input"
-            type="text"
-            className="border border-gray-400 p-3 rounded-lg w-full focus:outline-none focus:border-blue-500"
-            value={nymCode}
-            onChange={(e) => setNymCode(e.target.value)}
-          />
-          <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
-            <svg className="w-6 h-6 fill-current text-gray-400" viewBox="0 0 24 24">
-              <path d="M5.293 6.293a1 1 0 011.414 0L12 11.586l5.293-5.293a1 1 0 011.414 1.414L13.414 13l5.293 5.293a1 1 0 01-1.414 1.414L12 14.414l-5.293 5.293a1 1 0 01-1.414-1.414L10.586 13 5.293 7.707a1 1 0 010-1.414z" />
-            </svg>
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed mt-4"
-            onClick={signNymCode}
-            disabled={nymCode !== '' && !nymSig ? false : true}
-          >
-            Sign Nym code
-          </button>
-          <input
-            id="input"
-            type="text"
-            className="mt-4 border border-gray-400 p-3 rounded-lg w-full focus:outline-none focus:border-blue-500"
-            value={nymContentInput.parentId === '0x0' ? '' : nymContentInput.parentId}
-            placeholder="Parent ID (optional)"
-            onChange={(e) =>
-              setNymContentInput({
-                ...nymContentInput,
-                parentId: e.target.value as PrefixedHex,
-              })
-            }
-          />
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed mt-4"
-            onClick={postPseudo}
-            disabled={!nymSig ? true : false}
-          >
-            Submit a pseudonymous post
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
+//     console.log('Created a non-pseudonymous post! postId', result.data.postId);
+//   };
 
-export default ExamplePost;
+//   return (
+//     <div className="flex flex-row gap-16 mt-4 justify-center">
+//       <div>
+//         <p className="text-xl">Doxed post</p>
+//         <p className="text-gray-700 font-bold mb-2 block mt-4">Your address</p>
+//         <p className="text-gray-700 font-bold mb-2 block">{hydrated && address}</p>
+//         <button
+//           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+//           onClick={postDoxed}
+//         >
+//           Submit a doxed post
+//         </button>
+//       </div>
+
+//       <div>
+//         <p className="text-xl">Pseudonymous post</p>
+//         <div className="relative mt-4">
+//           <label className="text-gray-700 font-bold mb-2 block">Your Nym</label>
+//           <input
+//             id="input"
+//             type="text"
+//             className="border border-gray-400 p-3 rounded-lg w-full focus:outline-none focus:border-blue-500"
+//             value={nymCode}
+//             onChange={(e) => setNymCode(e.target.value)}
+//           />
+//           <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+//             <svg className="w-6 h-6 fill-current text-gray-400" viewBox="0 0 24 24">
+//               <path d="M5.293 6.293a1 1 0 011.414 0L12 11.586l5.293-5.293a1 1 0 011.414 1.414L13.414 13l5.293 5.293a1 1 0 01-1.414 1.414L12 14.414l-5.293 5.293a1 1 0 01-1.414-1.414L10.586 13 5.293 7.707a1 1 0 010-1.414z" />
+//             </svg>
+//           </div>
+//         </div>
+//         <div className="flex flex-col">
+//           <button
+//             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed mt-4"
+//             onClick={signNymCode}
+//             disabled={nymCode !== '' && !nymSig ? false : true}
+//           >
+//             Sign Nym code
+//           </button>
+//           <button
+//             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed mt-4"
+//             onClick={postPseudo}
+//             disabled={!nymSig ? true : false}
+//           >
+//             Submit a pseudonymous post
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ExamplePost;
