44206391a5f5ae9d53cf7b11a32b0955a9f8b99e
Daniel Tehrani <contact@dantehrani.com>
2023-05-12 12:29:33 +0200

    fix: broken commands

--- scripts/setup_merkle_cron.sh
+++ scripts/setup_merkle_cron.sh
@@ -1,17 +1,19 @@
 # Script to setup the merkle cron job in Render
 
 # 1. Install dependencies for the packages we need
-pnpm -F db nym-merkle-cron --prod i && 
+pnpm -F db i --ignore-script && 
+pnpm -F merkle_tree i --ignore-scripts && 
 
 # 2. Build in db generates the Prisma artifacts, 
-# and build in nym-merkle-cron generates the Graph client
-pnpm -F db nym-merkle-cron build
+# and build in merkle_tree generates the Graph client
+pnpm -F db build &&
+pnpm -F merkle_tree build &&
 
 # 3. Run database migrations
 # We also run database migrations in Github Actions,
 # but we need to run them here as well to ensure that
 # the database is up to date before we populate the cache
-pnpm -F db run migrate:prod
+pnpm -F db run migrate:prod &&
 
 # 4. Populate database with cache
-pnpm -F nym-merkle-tree populateCache
+pnpm -F merkle_tree run populateCache
