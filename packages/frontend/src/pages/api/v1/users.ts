cb2a52703623258072ae097cbac0d8114d479a11
cha0sg0d <0xcha0sg0d@gmail.com>
2023-05-11 17:09:13 -0700

    chore: outline users page route

---
+++ packages/frontend/src/pages/api/v1/users.ts
@@ -0,0 +1,11 @@
+// TODO: Prisma group by query that performs user stats based on group by + count.
+/**
+ * End goal is data like:
+ * {
+ *  userId:
+ *  numPosts:
+ *  numUpvotes:
+ *  timeLastActive:
+ *  numReplies:
+ * }
+ */
