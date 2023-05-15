762beb5af880727a243acd9340f4a5e3b4583841
Grace Fagan <gfagan01@tufts.edu>
2023-05-15 08:58:42 -0700

    feat: plumbing

--- packages/frontend/src/components/userInput/NewNym.tsx
+++ packages/frontend/src/components/userInput/NewNym.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import { Modal } from '../global/Modal';
 import { useState } from 'react';
 import { MainButton } from '../MainButton';
+import { NYM_CODE_TYPE, DOMAIN } from '@personaelabs/nymjs';
+import { useSignTypedData } from 'wagmi';
 
 interface NewNymProps {
   isOpen: boolean;
@@ -10,10 +12,17 @@ interface NewNymProps {
 
 export const NewNym = (props: NewNymProps) => {
   const { isOpen, handleClose } = props;
-
-  const confirmNewNym = () => console.log('confirm new nym');
-
   const [nymName, setNymName] = useState<string>('');
+
+  const { data: nymSig, signTypedData: signNymCode } = useSignTypedData({
+    primaryType: 'Nym',
+    domain: DOMAIN,
+    types: NYM_CODE_TYPE,
+    message: {
+      nymName,
+    },
+  });
+
   return (
     <Modal width="50%" isOpen={isOpen} handleClose={handleClose}>
       <div className="flex flex-col gap-4 py-8 px-12 md:px-12 md:py-10">
@@ -23,10 +32,11 @@ export const NewNym = (props: NewNymProps) => {
         <p className="text-gray-700">
           What does it mean to create a new nym? Any warnings the user should know beforehand?
         </p>
-        <div className="flex justify-start gap-2">
-          <Image alt={'profile'} src={'/anon-noun.png'} width={20} height={20} />
-          <div className="w-40 relative border border-gray-200 rounded-md px-4 py-2.5">
+        <div className="flex justify-start items-center gap-2">
+          <Image alt={'profile'} src={'/anon-noun.png'} width={24} height={24} />
+          <div className="relative border border-gray-200 rounded-md px-2 py-1">
             <input
+              className="outline-none bg-transparent"
               type="text"
               placeholder="Name"
               value={nymName}
@@ -36,7 +46,13 @@ export const NewNym = (props: NewNymProps) => {
           <p className="secondary">#0000</p>
         </div>
         <div className="flex justify-center">
-          <MainButton color="#0E76FD" message="Confirm" loading={false} handler={confirmNewNym} />
+          <MainButton
+            color="#0E76FD"
+            message="Confirm"
+            loading={false}
+            handler={signNymCode}
+            disabled={nymName !== '' && !nymSig ? false : true}
+          />
         </div>
       </div>
     </Modal>
