a2332c00016c5574accb10f68328ee38d85a12cd
cha0sg0d <0xcha0sg0d@gmail.com>
2023-05-15 17:18:11 -0700

    chore: remove test client

--- packages/test_client/types/circomlibjs/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "circomlibjs" {
-  export function buildPoseidon(): Promise<any>;
-
-  export function poseidon(inputs: any): any;
-
-  export function poseidon_slow(inputs: any): any;
-}
