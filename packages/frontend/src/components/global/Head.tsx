d670bfd7ef6fdf63f2a039d0e7f7546719f862ff
cha0sg0d <0xcha0sg0d@gmail.com>
2023-05-17 15:07:21 -0700

    chore: favicon and head

---
+++ packages/frontend/src/components/global/Head.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+function HeadHtml() {
+  return (
+    <div>
+      <Head>
+        <title>Noun Nyms</title>
+        <link type="favicon" rel="icon" href="/favicon-3.ico" />
+      </Head>
+    </div>
+  );
+}
+
+export default HeadHtml;
