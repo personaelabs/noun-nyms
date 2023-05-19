249aba2b36106d4a1cdda4895fbd0b00bd25ecfd
Grace Fagan <gfagan01@tufts.edu>
2023-05-19 10:29:12 -0400

    style: template tweaks

--- packages/frontend/src/components/post/PostPreview.tsx
+++ packages/frontend/src/components/post/PostPreview.tsx
@@ -29,7 +29,12 @@ export const PostPreview = (postProps: PostProps) => {
         className="rounded-2xl transition-all shadow-sm bg-white p-3 md:px-5 md:py-4 flex flex-col gap-4 justify-between border border-gray-200 hover:border-gray-300 hover:cursor-pointer w-full"
       >
         {root ? (
-          <p className="postDetail">{root.title}</p>
+          <div className="flex flex-col gap-2">
+            <p className="postDetail">{root.title}</p>
+            <p className="secondary">
+              Posted by <strong>{root.userId}</strong>
+            </p>
+          </div>
         ) : (
           <h4 className="tracking-tight">{title}</h4>
         )}
@@ -37,25 +42,24 @@ export const PostPreview = (postProps: PostProps) => {
           <>
             <UserTag userId={parent.userId} timestamp={parent.timestamp} />
             <span>{parent.body}</span>
-            <div
-              className="flex flex-col gap-2 pl-2 border-l border-dotted border-gray-300"
-              style={{ marginLeft: parent ? 20 : 0 }}
-            >
-              <div className="self-start line-clamp-2"></div>
-              <UserTag userId={userId} timestamp={timestamp} />
-              <span>{body}</span>
-              <div className="flex gap-2 justify-between">
-                <Upvote upvotes={upvotes} postId={id} onSuccess={onSuccess}>
-                  <p className="postDetail">{upvotes.length}</p>
-                </Upvote>
-                <div className="flex gap-2">
-                  <ReplyCount count={_count.descendants} />
-                  <div
-                    className="flex gap-2 items-center cursor-pointer hoverIcon"
-                    onClick={handleOpenPost}
-                  >
-                    <FontAwesomeIcon className="hoverIcon" icon={faReply} />
-                    <p className="text-gray-700">Reply</p>
+            <div className="p-4 rounded-xl bg-gray-50" style={{ marginLeft: 20 }}>
+              <div className="flex flex-col gap-2 pl-2 border-l border-dotted border-gray-300">
+                <div className="self-start line-clamp-2"></div>
+                <UserTag userId={userId} timestamp={timestamp} />
+                <span>{body}</span>
+                <div className="flex gap-2 justify-between">
+                  <Upvote upvotes={upvotes} postId={id} onSuccess={onSuccess}>
+                    <p className="postDetail">{upvotes.length}</p>
+                  </Upvote>
+                  <div className="flex gap-2">
+                    <ReplyCount count={_count.descendants} />
+                    <div
+                      className="flex gap-2 items-center cursor-pointer hoverIcon"
+                      onClick={handleOpenPost}
+                    >
+                      <FontAwesomeIcon className="hoverIcon" icon={faReply} />
+                      <p className="text-gray-700">Reply</p>
+                    </div>
                   </div>
                 </div>
               </div>
