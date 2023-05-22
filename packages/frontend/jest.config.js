ec5dbda2dd50c411b9dec7e662abd6ff8a3fe320
Daniel Tehrani <contact@dantehrani.com>
2023-05-22 12:52:22 +0200

    feat: unit tests for all POST endpoints

---
+++ packages/frontend/jest.config.js
@@ -0,0 +1,16 @@
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  moduleNameMapper: {
+    '@/lib/prisma': ['<rootDir>/src/lib/prisma'],
+    '@/lib/errors': ['<rootDir>/src/lib/errors'],
+    '@/types/api': ['<rootDir>/types/api'],
+  },
+  testTimeout: 600000,
+  setupFilesAfterEnv: ['<rootDir>/test/prisma.mock.ts'],
+};
--- packages/frontend/src/pages/api/v1/posts/[postId]/upvote.ts
+++ packages/frontend/src/pages/api/v1/posts/[postId]/upvote.ts
@@ -2,26 +2,49 @@ import prisma from '@/lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { pubToAddress } from '@ethereumjs/util';
 import { PrefixedHex, recoverUpvotePubkey, toUpvote } from '@personaelabs/nymjs';
-import { verifyInclusion } from '../../utils';
+import { upvoteExists, verifyInclusion } from '../../utils';
+import {
+  ALREADY_UPVOTED,
+  INVALID_SIGNATURE,
+  USER_NOT_IN_LATEST_GROUP,
+  INVALID_TIMESTAMP,
+} from '@/lib/errors';
+import { isTimestampValid } from '../../utils';
 
 // Handle non-pseudonymous upvotes
 // Verify the ECDSA signature and save the upvote
-const handleUpvote = async (req: NextApiRequest, res: NextApiResponse) => {
+const handleUpvote = async (req: NextApiRequest, res: NextApiResponse<{} | { error: string }>) => {
   const postId = req.query.postId as PrefixedHex;
   const timestamp = req.body.timestamp;
   const sig = req.body.sig;
   const groupRoot = req.body.groupRoot;
 
+  if (!isTimestampValid(timestamp)) {
+    res.status(400).send({ error: INVALID_TIMESTAMP });
+    return;
+  }
+
   const upvote = toUpvote(postId, groupRoot, timestamp, sig);
 
-  const pubKey = recoverUpvotePubkey(upvote);
-
-  if (!(await verifyInclusion(pubKey))) {
-    res.status(400).send('Public key not in latest group');
+  let pubKey;
+  try {
+    pubKey = recoverUpvotePubkey(upvote);
+  } catch (_err) {
+    res.status(400).send({ error: INVALID_SIGNATURE });
     return;
   }
 
   const address = `0x${pubToAddress(Buffer.from(pubKey.replace('0x', ''), 'hex')).toString('hex')}`;
+
+  if (await upvoteExists(postId, address)) {
+    res.status(400).send({ error: ALREADY_UPVOTED });
+    return;
+  }
+
+  if (!(await verifyInclusion(pubKey))) {
+    res.status(400).send({ error: USER_NOT_IN_LATEST_GROUP });
+    return;
+  }
 
   await prisma.doxedUpvote.create({
     data: {
