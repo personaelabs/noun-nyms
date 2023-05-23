3d5a017ef64a2efc87dc1b6fa49e2fe55a95f2ec
Grace Fagan <gfagan01@tufts.edu>
2023-05-23 15:03:19 -0400

    fix: fontawesome css

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
-
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import { WagmiConfig, createConfig, mainnet } from 'wagmi';
 import { createPublicClient, http } from 'viem';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Head from '@/components/global/Head';
 
-const config = createConfig({
+config.autoAddCss = false;
+
+const appConfig = createConfig({
   autoConnect: true,
   publicClient: createPublicClient({
     chain: mainnet,
@@ -20,7 +23,7 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <WagmiConfig config={config}>
+      <WagmiConfig config={appConfig}>
         <Head />
         <Component {...pageProps} />
       </WagmiConfig>
