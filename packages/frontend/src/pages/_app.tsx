25def33a1efed235ef336c80596bb16bc8b5bbeb
cha0sg0d <0xcha0sg0d@gmail.com>
2023-05-23 14:32:34 -0400

    fix: favicon

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '@/styles/globals.css';
 import { WagmiConfig, createConfig, mainnet } from 'wagmi';
 import { createPublicClient, http } from 'viem';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Head from '@/components/global/Head';
 
 const config = createConfig({
   autoConnect: true,
@@ -20,6 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <WagmiConfig config={config}>
+        <Head />
         <Component {...pageProps} />
       </WagmiConfig>
     </QueryClientProvider>
