18f7aa64c1fd1d65937ceb99a2adc11893ed14ac
cha0sg0d <0xcha0sg0d@gmail.com>
2023-05-24 13:21:12 -0400

    chore: sad boi

--- packages/frontend/src/components/global/Head.tsx
+++ packages/frontend/src/components/global/Head.tsx
@@ -6,7 +6,7 @@ function HeadHtml() {
       <Head>
         <title>Noun Nyms</title>
         <link type="favicon" rel="icon" href="/favicon-3.ico" />
-        <meta property="og:image" content="/nouns_og.jpg" />
+        <meta property="og:image" content="/noun_og.jpg" />
       </Head>
     </div>
   );
