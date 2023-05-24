46d0a6c577878556e0cb99f822769678cd29c44b
cha0sg0d <0xcha0sg0d@gmail.com>
2023-05-24 13:53:43 -0400

    chore: better description

--- packages/frontend/src/components/global/Head.tsx
+++ packages/frontend/src/components/global/Head.tsx
@@ -6,7 +6,13 @@ function HeadHtml() {
       <Head>
         <title>Noun Nyms</title>
         <link type="favicon" rel="icon" href="/favicon-3.ico" />
-        <meta property="og:image" content="/noun_og.jpg" />
+        <meta property="description" content="Where the truth comes out." />
+        <meta
+          property="og:image"
+          content="https://nym-git-cha0s-link-personaelabs.vercel.app/noun_og.jpg"
+        />
+        <meta property="og:title" content="Noun Nyms" />
+        <meta property="og:description" content="Where the truth comes out." />
       </Head>
     </div>
   );
