61f08f567c109e47d426f7865c4eabbdf88e35ad
cha0sg0d <0xcha0sg0d@gmail.com>
2023-05-28 19:56:27 -0400

    fix: refresh bug (but unsure why this works still uses example)

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -2,21 +2,28 @@ import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { WagmiConfig, createConfig, mainnet } from 'wagmi';
-import { createPublicClient, http } from 'viem';
+import { WagmiConfig, configureChains, createConfig, mainnet } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { HOME_DESCRIPTION, Seo, TITLE } from '@/components/global/Seo';
 import Head from 'next/head';
 import { ValidUserWarning } from '@/components/global/ValidUserWarning';
+import { getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { polygon, optimism } from 'viem/chains';
 
 config.autoAddCss = false;
 
+const { chains, publicClient } = configureChains([mainnet, polygon, optimism], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains,
+});
+
 const appConfig = createConfig({
   autoConnect: true,
-  publicClient: createPublicClient({
-    chain: mainnet,
-    transport: http(),
-  }),
+  connectors,
+  publicClient,
 });
 
 // React query client
