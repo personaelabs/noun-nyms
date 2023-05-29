67c540f1827500bfc6bb7ffd760aa057fbc6467c
Grace Fagan <gfagan01@tufts.edu>
2023-05-29 16:06:48 -0400

    refactor: valid user warning bug

--- packages/frontend/src/components/global/ValidUserWarning.tsx
+++ packages/frontend/src/components/global/ValidUserWarning.tsx
@@ -8,9 +8,11 @@ import { UserContextType } from '@/types/components';
 export const ValidUserWarning = () => {
   const { address } = useAccount();
   const { isValid } = useContext(UserContext) as UserContextType;
-  const [showWarning, setShowWarning] = useState<boolean>(!isValid);
+  const [showWarning, setShowWarning] = useState<boolean>(false);
 
-  useEffect(() => setShowWarning(!isValid), [address, isValid]);
+  // disable warning when account changes and then reset it once isValid changes
+  useEffect(() => setShowWarning(false), [address]);
+  useEffect(() => setShowWarning(!isValid), [isValid]);
 
   return (
     <>
