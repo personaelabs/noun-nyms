8f19bdb1116a8af3e74359133992174378691aa4
Grace Fagan <gfagan01@tufts.edu>
2023-05-30 10:57:58 -0400

    feat: link to all users

--- packages/frontend/src/components/MyProfile.tsx
+++ packages/frontend/src/components/MyProfile.tsx
@@ -4,6 +4,8 @@ import { ClientName, NameType, UserContextType } from '@/types/components';
 import useName from '@/hooks/useName';
 import { UserContext } from '@/pages/_app';
 import { useAccount } from 'wagmi';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 export const MyProfile = () => {
   const { address } = useAccount();
@@ -30,42 +32,52 @@ export const MyProfile = () => {
         <div className="relative cursor-pointer" onClick={() => setOpenProfile(!openProfile)}>
           <UserAvatar width={50} userId={localAddress} />
           {openProfile ? (
-            <div className="max-w-[150px] absolute top-full right-0 bg-white mt-2 border border-gray-200 rounded-xl cursor-pointer p-2">
-              <p className="secondary px-2">My identities</p>
-              <a href={`/users/${localAddress}`}>
-                <div className="w-full flex justify-between gap-2 px-2 py-2.5 rounded-xl hover:bg-gray-100">
-                  <div className="w-full flex justify-between items-center gap-2">
-                    <div className="flex gap-2 w-full">
-                      <UserAvatar type={NameType.DOXED} userId={localAddress} width={20} />
-                      <p className="shrink overflow-hidden text-ellipsis whitespace-nowrap">
-                        {name}
-                      </p>
+            <div className="max-w-[150px] absolute top-full right-0 bg-white mt-2 border border-gray-200 rounded-xl cursor-pointer">
+              <p className="secondary p-2">My identities</p>
+              <div className="border-b border-dotted border-gray-300">
+                <a href={`/users/${localAddress}`}>
+                  <div className="w-full flex justify-between gap-2 px-2 py-2.5 rounded-xl hover:bg-gray-100">
+                    <div className="w-full flex justify-between items-center gap-2">
+                      <div className="flex gap-2 w-full">
+                        <UserAvatar type={NameType.DOXED} userId={localAddress} width={20} />
+                        <p className="shrink overflow-hidden text-ellipsis whitespace-nowrap">
+                          {name}
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </a>
-              {nymOptions &&
-                nymOptions.map((nym) => (
-                  <div
-                    key={nym.nymSig}
-                    className="w-full flex justify-between gap-2 px-2 py-2.5 rounded-xl hover:bg-gray-100"
-                  >
-                    <a href={`/users/${getUserIdFromName(nym)}`}>
-                      <div className="w-full flex justify-between items-center gap-2">
-                        <div className="flex gap-2 w-full">
-                          <UserAvatar
-                            type={NameType.PSEUDO}
-                            userId={getUserIdFromName(nym)}
-                            width={20}
-                          />
-                          <p className="shrink overflow-hidden text-ellipsis whitespace-nowrap">
-                            {nym.name}
-                          </p>
+                </a>
+                {nymOptions &&
+                  nymOptions.map((nym) => (
+                    <div
+                      key={nym.nymSig}
+                      className="w-full flex justify-between gap-2 px-2 py-2.5 rounded-xl hover:bg-gray-100"
+                    >
+                      <a href={`/users/${getUserIdFromName(nym)}`}>
+                        <div className="w-full flex justify-between items-center gap-2">
+                          <div className="flex gap-2 w-full">
+                            <UserAvatar
+                              type={NameType.PSEUDO}
+                              userId={getUserIdFromName(nym)}
+                              width={20}
+                            />
+                            <p className="shrink overflow-hidden text-ellipsis whitespace-nowrap">
+                              {nym.name}
+                            </p>
+                          </div>
                         </div>
-                      </div>
-                    </a>
+                      </a>
+                    </div>
+                  ))}
+              </div>
+              <div className="w-full flex justify-between gap-2 px-2 py-2.5 rounded-xl hover:bg-gray-100">
+                <a href={`/users`}>
+                  <div className="flex items-center gap-2 w-full">
+                    <FontAwesomeIcon className="w-6" icon={faUser} />
+                    <p>All Users</p>
                   </div>
-                ))}
+                </a>
+              </div>
             </div>
           ) : null}
         </div>
