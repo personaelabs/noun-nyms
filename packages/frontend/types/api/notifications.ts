f15adaa1f37e929e0c4720498dd071c0fe9d7e46
cha0sg0d <0xcha0sg0d@gmail.com>
2023-06-07 21:49:42 -0700

    fix: hydration error

--- packages/frontend/types/api/notifications.ts
+++ packages/frontend/types/api/notifications.ts
@@ -1,34 +1,6 @@
-// Notifications for frontend
 import { IPostPreview } from './postPreviewSelect';
 import { IUserUpvote } from './userUpvotesSelect';
 
 // Either Upvote or Post
 
-export enum NotificationType {
-  Upvote,
-  DirectReply,
-  DiscussionReply,
-}
-
 export type RawNotifications = { upvotes: IUserUpvote[]; posts: IPostPreview[] };
-
-export interface BaseNotification {
-  read: boolean;
-  type: NotificationType;
-  id: string; // Either postId or upvoteId
-  postId: string; // Always postId
-  userId: string;
-  title: string; // notification Title.
-  body: string; // notification Body.
-  timestamp: Date;
-}
-
-export type UpvoteNotification = BaseNotification & {
-  type: NotificationType.Upvote;
-};
-
-export type ReplyNotification = BaseNotification & {
-  type: NotificationType.DirectReply | NotificationType.DiscussionReply;
-};
-
-export type Notification = UpvoteNotification | ReplyNotification;
