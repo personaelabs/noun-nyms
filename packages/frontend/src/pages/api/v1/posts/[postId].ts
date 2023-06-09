646cd6d87dd91986650b4dc460c7b9f1b33040b7
cha0sg0d <0xcha0sg0d@gmail.com>
2023-06-09 16:38:19 -0700

    chore: parent fetching works

--- packages/frontend/src/pages/api/v1/posts/[postId].ts
+++ packages/frontend/src/pages/api/v1/posts/[postId].ts
@@ -34,6 +34,26 @@ export const getRootAndPostIdFromPreview = (
     throw new Error(`post preview not found`);
   }
 };
+
+export const getPostWithReplies = async (id: string, fromRoot: boolean) => {
+  const postPreview = await getPostPreview(id);
+  const { id: postId, rootPost } = getRootAndPostIdFromPreview(fromRoot, postPreview);
+
+  const select = buildPostSelect(POST_DEPTH);
+
+  const postWithReplies = await prisma.post.findFirst({
+    select: select,
+    where: {
+      id: postId,
+    },
+  });
+
+  if (!postWithReplies) throw new Error(`no post with replies found`);
+
+  const result: IPostWithReplies = { ...postWithReplies };
+  result.root = rootPost;
+  return result;
+};
 const handleGetPost = async (
   req: NextApiRequest,
   res: NextApiResponse<IPostWithReplies | { error: string }>,
@@ -47,25 +67,7 @@ const handleGetPost = async (
     return;
   }
 
-  const postPreview = await getPostPreview(id);
-  const { id: postId, rootPost } = getRootAndPostIdFromPreview(fromRoot, postPreview);
-
-  const select = buildPostSelect(POST_DEPTH);
-
-  const postWithReplies = await prisma.post.findFirst({
-    select: select,
-    where: {
-      id: postId,
-    },
-  });
-
-  if (!postWithReplies) {
-    res.status(404).send({ error: 'Post not found' });
-    return;
-  }
-
-  const result: IPostWithReplies = { ...postWithReplies };
-  if (rootPost) result.root = rootPost;
+  const result = await getPostWithReplies(id, fromRoot);
 
   res.send(result);
 };
--- packages/frontend/src/pages/api/v1/posts/[postId]/parents.ts
+++ packages/frontend/src/pages/api/v1/posts/[postId]/parents.ts
@@ -9,6 +9,7 @@ import {
   IPostWithParents,
 } from '@/types/api';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getPostWithReplies } from '../[postId]';
 
 // Return a single post and all of its replies till depth = 5
 // TODO: Return all replies with a much higher depth limit
@@ -16,7 +17,6 @@ const handleGetParents = async (
   req: NextApiRequest,
   res: NextApiResponse<IPostWithReplies | { error: string }>,
 ) => {
-  const fromRoot = !!(req.query.fromRoot && req.query.fromRoot == 'true');
   let id = req.query.postId as string;
 
   // Before we do anything, get the parents of a post
@@ -38,57 +38,12 @@ const handleGetParents = async (
     }
     console.log(`found parent at depth`, currPost.depth);
     // Now run the logic to get the posts + children of curr post.
-    res.send(parentPostWithReplies);
+    const result = await getPostWithReplies(currPost.id, false);
+    res.send(result);
   } else {
     res.status(404).send({ error: 'Parents not found' });
     return;
   }
-
-  // If fromRoot is true, we get the rootId of the given post
-  // let rootPost: IPostPreview['root'] | undefined = undefined;
-
-  // // First get postPreview.
-  // const postPreview = await prisma.post.findFirst({
-  //   select: postPreviewSelect,
-  //   where: {
-  //     id,
-  //   },
-  // });
-
-  // // TODO: explain logic better
-  // if (postPreview) {
-  //   const { root, ...post } = postPreview;
-  //   // topContent now contains the root.
-  //   const topContent = root || post;
-  //   rootPost = topContent;
-  //   // Post id to fetch is the root by default.
-  //   id = topContent.id;
-  //   // If fromRoot is true and depth > POST_DEPTH, we return postID
-  //   if (fromRoot && postPreview.depth > POST_DEPTH) {
-  //     id = post.id;
-  //   }
-  //   // However, if we don't want to fetch from the root, we maintain the original postId.
-  //   if (!fromRoot) id = post.id;
-  // }
-
-  // const select = buildPostSelect(POST_DEPTH);
-
-  // const postWithReplies = await prisma.post.findFirst({
-  //   select: select,
-  //   where: {
-  //     id,
-  //   },
-  // });
-
-  // if (!postWithReplies) {
-  //   res.status(404).send({ error: 'Post not found' });
-  //   return;
-  // }
-
-  // const result: IPostWithReplies = { ...postWithReplies };
-  // if (rootPost) result.root = rootPost;
-
-  // res.send(result);
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
