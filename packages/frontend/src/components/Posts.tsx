8f11159d36ed3c7770489f155e651e6cc6af6e91
cha0sg0d <0xcha0sg0d@gmail.com>
2023-06-11 09:46:37 -0700

    fix: filters stay in view when loading

--- packages/frontend/src/components/Posts.tsx
+++ packages/frontend/src/components/Posts.tsx
@@ -110,34 +110,34 @@ export default function Posts(props: PostsProps) {
         <div className="w-full bg-gray-50 flex flex-col justify-center items-center">
           <div className="bg-gray-50 min-h-screen w-full">
             <div className="flex flex-col gap-8 max-w-3xl mx-auto py-5 md:py-10 px-4 md:px-0">
+              <div className="flex justify-between">
+                {isMobile ? (
+                  <SortSelect
+                    options={filterOptions}
+                    selectedQuery={filter}
+                    setSelectedQuery={setFilter}
+                  />
+                ) : (
+                  <Filters
+                    filters={filterOptions}
+                    selectedFilter={filter}
+                    setSelectedFilter={setFilter}
+                  />
+                )}
+                <div className="grow-0">
+                  <MainButton
+                    color="#0E76FD"
+                    message="Start Discussion"
+                    handler={() => setNewPostOpen(true)}
+                  />
+                </div>
+              </div>
               {isLoading ? (
                 <>
                   <Spinner />
                 </>
               ) : data?.pages ? (
                 <>
-                  <div className="flex justify-between">
-                    {isMobile ? (
-                      <SortSelect
-                        options={filterOptions}
-                        selectedQuery={filter}
-                        setSelectedQuery={setFilter}
-                      />
-                    ) : (
-                      <Filters
-                        filters={filterOptions}
-                        selectedFilter={filter}
-                        setSelectedFilter={setFilter}
-                      />
-                    )}
-                    <div className="grow-0">
-                      <MainButton
-                        color="#0E76FD"
-                        message="Start Discussion"
-                        handler={() => setNewPostOpen(true)}
-                      />
-                    </div>
-                  </div>
                   {data.pages.map((page, i) => (
                     <>
                       {page.map((post, j) => (
