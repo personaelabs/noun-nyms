cf1a3f8ae005feeaedf5064679d966a4b96fad16
Grace Fagan <gfagan01@tufts.edu>
2023-06-12 11:16:25 -0400

    fix: header loading bug

--- packages/frontend/src/components/Header.tsx
+++ packages/frontend/src/components/Header.tsx
@@ -2,7 +2,7 @@ import ConnectWallet from './ConnectWallet';
 import logo from '../../public/logo.svg';
 import { MyProfile } from './MyProfile';
 import Image from 'next/image';
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { UserContext } from '@/pages/_app';
 import { UserContextType } from '@/types/components';
 import { useAccount } from 'wagmi';
@@ -17,34 +17,36 @@ export const Header = () => {
 
   return (
     <>
-      <div className="bg-black dots w-full">
-        <div className="flex flex-col gap-2 py-4 md:pt-6 md:pb-0">
-          <nav className="w-full px-4 md:px-6 flex justify-between items-center">
-            <div
-              className="min-w-0 flex shrink gap-2 items-center cursor-pointer"
-              onClick={() => pushRoute('/')}
-            >
-              <div>
-                <div className="w-[35px] md:w-auto">
-                  <Image src={logo} alt="logo" />
+      {isMobile !== null && (
+        <div className="bg-black dots w-full">
+          <div className="flex flex-col gap-2 py-4 md:pt-6 md:pb-0">
+            <nav className="w-full px-4 md:px-6 flex justify-between items-center">
+              <div
+                className="min-w-0 flex shrink gap-2 items-center cursor-pointer"
+                onClick={() => pushRoute('/')}
+              >
+                <div>
+                  <div className="w-[35px] md:w-auto">
+                    <Image src={logo} alt="logo" />
+                  </div>
                 </div>
+                {!isMobile && <h3 className="text-white font-semibold breakText">PseudoNym</h3>}
               </div>
-              {!isMobile && <h3 className="text-white font-semibold breakText">PseudoNym</h3>}
-            </div>
-            <div className="flex gap-4 items-center">
-              {isMounted && address && isValid && <Notifications />}
-              {(!isMobile || !address || !isValid) && <ConnectWallet />}
-              {isMounted && address && <MyProfile address={address} />}
-            </div>
-          </nav>
-          {!isMobile && (
-            <div className="grow flex justify-center">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img className="w-[100px] md:w-[150px]" src="/nouns.png" alt="nouns" />
-            </div>
-          )}
+              <div className="flex gap-4 items-center">
+                {isMounted && address && isValid && <Notifications />}
+                {(!isMobile || !address || !isValid) && <ConnectWallet />}
+                {isMounted && address && <MyProfile address={address} />}
+              </div>
+            </nav>
+            {!isMobile && (
+              <div className="grow flex justify-center">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img className="w-[100px] md:w-[150px]" src="/nouns.png" alt="nouns" />
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
