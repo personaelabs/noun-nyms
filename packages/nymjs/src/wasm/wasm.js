7a1fdb0d32a1678850d9832549545ba73acd9e23
Lakshman Sankar <lsankar4033@gmail.com>
2023-06-21 07:46:10 -0700

    Revert "[WIP] Fix out of memory error (#200)"
    This reverts commit 04c75ab6c5ae8268a791399574395037bfd9937d.

--- packages/nymjs/src/wasm/wasm.js
+++ packages/nymjs/src/wasm/wasm.js
@@ -433,9 +433,8 @@ function __wbg_get_imports() {
 }
 
 function __wbg_init_memory(imports, maybe_memory) {
-  imports.wbg.memory = maybe_memory || new WebAssembly.Memory({ initial: 18, maximum: 8192 });
-  // initial ~= 1.18MB
-  // maximum ~= 569MB
+  imports.wbg.memory =
+    maybe_memory || new WebAssembly.Memory({ initial: 18, maximum: 16384, shared: true });
 }
 
 function __wbg_finalize_init(instance, module) {
