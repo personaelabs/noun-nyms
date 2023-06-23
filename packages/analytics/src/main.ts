56efbfd0e0d133084be40545ed2553c33a51e7ed
Daniel Tehrani <contact@dantehrani.com>
2023-06-23 11:56:38 +0900

    chore: cleanup

--- packages/analytics/src/main.ts
+++ packages/analytics/src/main.ts
@@ -6,44 +6,51 @@ const prisma = new PrismaClient();
 const BLOCK_TIME = 12; // seconds
 const DAY_IN_SECONDS = 24 * 60 * 60;
 
+// Get the delta of the oldest set and the latest set,
+// and calculate the speed of the pseudonymity set size reduction.
 const main = async () => {
-  const startTree = await prisma.tree.findFirstOrThrow({
+  // Get the oldest and latest trees
+  const oldestTree = await prisma.tree.findFirstOrThrow({
     orderBy: { blockHeight: "asc" }
   });
-  const endTree = await prisma.tree.findFirstOrThrow({
+  const latestTree = await prisma.tree.findFirstOrThrow({
     orderBy: { blockHeight: "desc" }
   });
-
-  const startTreeNodes = await prisma.treeNode.findMany({
+  const oldestTreeNodes = await prisma.treeNode.findMany({
     where: {
-      root: startTree.root
+      root: oldestTree.root
+    }
+  });
+  const latestTreeNodes = await prisma.treeNode.findMany({
+    where: {
+      root: latestTree.root
     }
   });
 
-  const endTreeNodes = await prisma.treeNode.findMany({
-    where: {
-      root: endTree.root
-    }
-  });
+  // Get the set of addresses in the oldest and latest trees
+  const oldestSet = oldestTreeNodes.map(node => node.address);
+  const latestSet = latestTreeNodes.map(node => node.address);
 
-  // Get the number of removed nodes
-  const removedNodes = startTreeNodes.filter(startTreeNode => {
-    return !endTreeNodes.some(
-      endTreeNode => endTreeNode.address === startTreeNode.address
-    );
-  });
+  // Get the set of removed addresses
+  const deltaSet = oldestSet.filter(address => !latestSet.includes(address));
 
-  const blockHeightDiff = endTree.blockHeight - startTree.blockHeight;
+  // Calculate the speed of the pseudonymity set size reduction
+  const blockHeightDiff = latestTree.blockHeight - oldestTree.blockHeight;
   const duration = ((blockHeightDiff * BLOCK_TIME) / DAY_IN_SECONDS).toFixed(2);
-  const setSizeReductionPerBlock = removedNodes.length / blockHeightDiff;
+  const setSizeReductionPerBlock = deltaSet.length / blockHeightDiff;
   const setSizeReductionPerDay = (
     (setSizeReductionPerBlock / BLOCK_TIME) *
     DAY_IN_SECONDS
   ).toFixed(2);
 
+  // Print the result
   console.log(
-    `From block ${startTree.blockHeight} to block ${endTree.blockHeight} (duration of ${duration} days), ${removedNodes.length} members were removed.`
+    `From block ${oldestTree.blockHeight} to block ${latestTree.blockHeight} (duration of ${duration} days), ${deltaSet.length} members were removed.`
   );
+  console.log(`Removed members:`);
+  for (let i = 0; i < deltaSet.length; i++) {
+    console.log(deltaSet[i]);
+  }
   console.log("Member reduction per day:", setSizeReductionPerDay);
 };
 
