161df7ec699b037a7d979a0834caa32ba1987771
Daniel Tehrani <contact@dantehrani.com>
2023-06-29 16:46:29 +0900

    chore: cleanup

---
+++ packages/analytics/README.md
@@ -0,0 +1,6 @@
+1. Set the environment variable DATABASE_URL to be the read-replica of the production database.
+2. Run the following command
+
+```
+pnpm main
+```
--- packages/analytics/src/main.ts
+++ packages/analytics/src/main.ts
@@ -9,6 +9,10 @@ const DAY_IN_SECONDS = 24 * 60 * 60;
 // Get the delta of the oldest set and the latest set,
 // and calculate the speed of the pseudonymity set size reduction.
 const main = async () => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("env var DATABASE_URL is not set");
+  }
+
   // Get the oldest and latest trees
   const oldestTree = await prisma.tree.findFirstOrThrow({
     orderBy: { blockHeight: "asc" }
@@ -52,6 +56,11 @@ const main = async () => {
     console.log(deltaSet[i]);
   }
   console.log("Member reduction per day:", setSizeReductionPerDay);
+  console.log(
+    `Pseudonymity set size reduced from ${oldestSet.length} to ${
+      oldestSet.length - deltaSet.length
+    }`
+  );
 };
 
 main();
