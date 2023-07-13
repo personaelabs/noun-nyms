597fd38d8f5a0b33a106d24de9d4ec84fd5b9e69
Lakshman Sankar <lsankar4033@gmail.com>
2023-07-13 16:05:48 +0100

    fix: simplify tweet format (#242)

--- packages/frontend/src/lib/post-tweet.ts
+++ packages/frontend/src/lib/post-tweet.ts
@@ -3,5 +3,5 @@ import { clientFactory } from './twitter';
 export const postTweet = async (postId: string, user: string) => {
   const url = process.env.NEXT_PUBLIC_SITE_URL + '/posts/' + postId;
   const client = clientFactory();
-  await client.v2.tweet(`New Post!\nAuthor: ${user}\nLink: ${url}`);
+  await client.v2.tweet(url);
 };
